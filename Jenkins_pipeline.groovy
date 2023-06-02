1beadfd2850490d6a8d7e5e739610e045c512d33
GOMATHISANKAR22 <117799775+GOMATHISANKAR22@users.noreply.github.com>
2023-06-02 06:28:58 +0100

    Update Jenkins_pipeline.groovy

--- Jenkins_pipeline.groovy
+++ Jenkins_pipeline.groovy
@@ -1,31 +1,46 @@
 pipeline {
     agent any
+    environment {
+        Git_Hub_URL             = 'https://github.com/GOMATHISANKAR22/DockerFile.git'
+        Workspace_name          = 'test1'
+        Cloudformation_Template = 'ECS-Fargate.yml'
+        Bucket_Name             = 'awsstoragedeploy123'
+        Image_Name              = 'test1'
+        Region_Name             = 'us-east-1'
+        Aws_Id                  = '811187436843'
+        Stack_Name              = 'my-stack'
+    }
+    
     stages {
         stage('Clone') {
             steps {
-                git branch: 'main', credentialsId: 'edb9612e-f882-4b86-ae44-cae1cadd438a', url: 'https://github.com/GOMATHISANKAR22/DockerFile.git'
+                git branch: 'main', credentialsId: 'edb9612e-f882-4b86-ae44-cae1cadd438a', url: "${Git_Hub_URL}"
             }
         }
         stage('Copy to S3') {
             steps {
-                sh 'aws s3 cp /var/lib/jenkins/workspace/test1/ECS-Fargate.yml s3://s3.artifactjenkins/'
+                sh '''
+                aws s3 mb s3://${Bucket_Name}
+                aws s3api put-bucket-versioning --bucket ${Bucket_Name} --versioning-configuration Status=Enabled
+                aws s3 cp /var/lib/jenkins/workspace/${Workspace_name}/${Cloudformation_Template} s3://${Bucket_Name}/ 
+                '''
             }
         }
         stage('Build') {
             steps {
                 sh '''
-                cd /var/lib/jenkins/workspace/test2
-                sudo docker build -t test1 .
+                cd /var/lib/jenkins/workspace/${Workspace_name}
+                sudo docker build -t ${Image_Name} .
                 '''
             }
         }
         stage('Push') {
             steps {
                 sh '''
-                sudo aws ecr create-repository --repository-name test1 --region us-east-1 || true  # Ignore error if repo already exists    
-                sudo aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 811187436843.dkr.ecr.us-east-1.amazonaws.com
-                sudo docker tag test1:latest 811187436843.dkr.ecr.us-east-1.amazonaws.com/test1:latest
-                sudo docker push 811187436843.dkr.ecr.us-east-1.amazonaws.com/test1:latest
+                sudo aws ecr create-repository --repository-name ${Image_Name} --region ${Region_Name} || true   
+                sudo aws ecr get-login-password --region ${Region_Name} | docker login --username AWS --password-stdin ${Aws_Id}.dkr.ecr.${Region_Name}.amazonaws.com
+                sudo docker tag ${Image_Name}:latest ${Aws_Id}.dkr.ecr.${Region_Name}.amazonaws.com/${Image_Name}:latest
+                sudo docker push ${Aws_Id}.dkr.ecr.${Region_Name}.amazonaws.com/${Image_Name}:latest 
                 '''
             }
         }
@@ -34,20 +49,18 @@ pipeline {
                 script {
                     def stackExists = sh(
                         returnStatus: true,
-                        script: 'aws cloudformation describe-stacks --stack-name my-stack'
+                        script: 'aws cloudformation describe-stacks --stack-name ${Stack_Name}'
                     )
                     if (stackExists == 0) {
-                        
                         script {
                             sh '''
-                            sudo aws cloudformation update-stack --stack-name my-stack --template-url https://s3.artifactjenkins.s3.amazonaws.com/ECS-Fargate.yml --capabilities CAPABILITY_NAMED_IAM  --parameters ParameterKey=ImageId,ParameterValue=811187436843.dkr.ecr.us-east-1.amazonaws.com/test1:latest
+                            sudo aws cloudformation update-stack --stack-name ${Stack_Name} --template-url https://${Bucket_Name}.s3.amazonaws.com/${Cloudformation_Template} --capabilities CAPABILITY_NAMED_IAM  --parameters ParameterKey=ImageUri,ParameterValue=${Aws_Id}.dkr.ecr.${Region_Name}.amazonaws.com/${Image_Name}:latest || true
                             '''
                         }
                     } else {
-                      
                         script {
                             sh '''
-                            sudo aws cloudformation create-stack --stack-name my-stack --template-url https://s3.artifactjenkins.s3.amazonaws.com/ECS-Fargate.yml --capabilities CAPABILITY_NAMED_IAM  --parameters ParameterKey=ImageId,ParameterValue=811187436843.dkr.ecr.us-east-1.amazonaws.com/test1:latest
+                            sudo aws cloudformation create-stack --stack-name ${Stack_Name} --template-url https://${Bucket_Name}.s3.amazonaws.com/${Cloudformation_Template} --capabilities CAPABILITY_NAMED_IAM  --parameters ParameterKey=ImageUri,ParameterValue=${Aws_Id}.dkr.ecr.${Region_Name}.amazonaws.com/${Image_Name}:latest
                             '''
                         }
                     }
@@ -59,3 +72,4 @@ pipeline {
 
 
         
+      
